38be7d087ba759e59d6824dc19ae289e3a721b16
simvar <simavark@gmail.com>
2012-10-09 17:52:49 +0200

    Patobulintas

--- main.cpp
+++ main.cpp
@@ -7,9 +7,11 @@ int main()
 
 
     cout << "Monte Carlo metodo realizacijos pradzia!!!" << endl;
+     cout << "(RANDOM SEARCH METHOD)" << endl;
     //Kintamuju apsirasimas
     double f,x; //Funkcijos f reksmes ir argumentas x
     int n; //uzdavinio dimencija
+    double xSprend; //Argumento
 
 
 
