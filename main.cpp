b517fe190d265732df6a932542958359e4410f8a
simvar <simavark@gmail.com>
2012-10-23 18:41:53 +0200

    Patobulinta

--- main.cpp
+++ main.cpp
@@ -21,26 +21,73 @@ int main()
 
     // Vektoriu kurimas, kuriu elementai yra atitiktiniai realus skaiciai
 
-    double virsutinis_rezis, apatinis_rezis; // apsirasome intervalo rezius
+    double virsutinis_rezis1, apatinis_rezis1,virsutinis_rezis2, apatinis_rezis2; // apsirasome intervalo rezius
 
-    cout << "Iveskite inervalo apatiny ir virsutiny rezius:" << endl;
+/*
+    cout << "Iveskite inervalo apatiny ir virsutiny rezius: ";
     cin >> apatinis_rezis >> virsutinis_rezis;
-    cout << "Iveskite vektoriaus dimensija (n):" << endl;
+    cout << "Iveskite vektoriaus dimensija (n): ";
     cin >> n;
-
+ */
+    apatinis_rezis1=-1.9;
+    virsutinis_rezis1=1.9;
+    apatinis_rezis2=-1.1;
+    virsutinis_rezis2=1.1;
+    double skaiciai[100];
+    double xx[100][2];
+for(int j=0;j<100;j++)
+{
+    n=2;
     double vektorius[n];
-    cout << "x = ( ";
-    for(int i=0; i<n; i++)
-    {
-        vektorius[i] = rand() * (virsutinis_rezis - apatinis_rezis) / RAND_MAX + apatinis_rezis;
-    }
+
+    cout << "x["<<j+1<<"] = ( ";
+    //for(int i=0; i<n; i++)
+    //{
+        vektorius[0] = rand() * (virsutinis_rezis1 - apatinis_rezis1) / RAND_MAX + apatinis_rezis1;
+        vektorius[1] = rand() * (virsutinis_rezis2 - apatinis_rezis2) / RAND_MAX + apatinis_rezis2;
+   // }
     for(int i=0; i<n; i++)
     {
         cout << vektorius[i] << " "; // Vektoriaus isvedimas i ekrana
     }
-    cout << ")" << endl;
-    cout << "Six hump funkcijos reiksme siame taske: " << sixhump(&vektorius[0]) << endl;
-    vektorius[0] = 0.089842; vektorius[1] = -0.7126546;
-    cout << "Six hump funkcijos reiksme minimumo taske: " << sixhump(&vektorius[0]) << endl;
+    cout << ") --> "<<sixhump(&vektorius[0])<< endl;
+    skaiciai[j]=sixhump(&vektorius[0]);
+    xx[j][0] = vektorius[0];
+    xx[j][1] = vektorius[1];
+    //cout << "Six hump funkcijos reiksme siame taske: " << sixhump(&vektorius[0]) << endl;
+   // system("pause>nul");
+}
+//-------------------------------------------
+// Paieðka
+double max = skaiciai[0]; // prielaida didþiausiam elementui
+double imax = 0; // jei pirmasis elementas toks ir bus – jo vieta yra 0
+ double ma1= xx[0][0];
+ double ma2= xx[0][1];
+double min = skaiciai[0]; // prielaidos maþiausiam elementui ...
+double imin = 0; // ir jo vietai masyve
+ double mi1= xx[0][0];
+ double mi2= xx[0][1];
+for( int i = 1; i < 100; i++ ){ // ciklà dabar galima pradëti nuo antrojo elemento
+if( skaiciai[i] > max ) {
+max = skaiciai[i];
+imax = i;
+  ma1= xx[i][0];
+  ma2= xx[i][1];
+}
+if( skaiciai[i] < min ) {
+min = skaiciai[i];
+imin = i;
+  mi1= xx[i][0];
+  mi2= xx[i][1];
+}
+}
+// Rezultatø spausdinimas
+cout<<"\nRezultatai\n";
+cout<<"Didziausias elementas yra "<<max<<", jo vieta yra "<<imax+1<<" x["<<imax+1<<"][0]="<<ma1<<" x["<<imax+1<<"][1]="<<ma2<<endl;
+cout<<"Maziausias elementas yra "<<min<<", jo vieta yra "<<imin+1<<" x["<<imin+1<<"][0]="<<mi1<<" x["<<imin+1<<"][1]="<<mi2<<endl;
+//
+//-----------------------------------------------
+    //vektorius[0] = 0.089842; vektorius[1] = -0.7126546;
+   // cout << "Six hump funkcijos reiksme minimumo taske: " << sixhump(&vektorius[0]) << endl;
     return 0;
 }
